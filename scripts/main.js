5e3d8fc0b05e1c20f06a30d34098e058fe568f9a
diorm47 <dior47m@gmail.com>
2024-10-29 00:02:02 +0500

    lang toggler

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,44 @@ function menuToggler() {
   menu.classList.toggle("mob_menu_wrapper_visible");
 }
 
+// Находим элементы
+const langToggler = document.querySelector('.lang_toggler');
+const langModal = document.querySelector('.lang_toggler_modal');
+const langItems = document.querySelectorAll('.lang_toggler_modal_item'); // Все элементы выбора языка
+
+// Обработчик клика для открытия/закрытия модалки и добавления класса активности
+langToggler.addEventListener('click', (event) => {
+  event.stopPropagation();
+  langToggler.classList.toggle('active_lang');
+});
+
+// Закрытие модалки при клике вне её области
+document.addEventListener('click', (event) => {
+  if (!langToggler.contains(event.target)) {
+    langToggler.classList.remove('active_lang');
+  }
+});
+
+// Обработчик для выбора элемента языка
+langItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Удаляем класс 'active' со всех элементов, чтобы только один был активен
+    langItems.forEach(i => i.classList.remove('lang_toggler_modal_item_active'));
+    item.classList.add('lang_toggler_modal_item_active'); // Добавляем класс активного элемента
+
+    // Обновляем флаг и текст в lang_toggler
+    const selectedFlag = item.querySelector('img').src;
+    const selectedText = item.querySelector('p').textContent;
+    langToggler.querySelector('img').src = selectedFlag;
+    langToggler.querySelector('p').textContent = selectedText;
+
+    // Закрываем модалку
+    langToggler.classList.remove('active_lang');
+  });
+});
+
+
+
 const decButton = document.querySelector(".dec");
 const incButton = document.querySelector(".inc");
 const depositValue = document.querySelector(".deposit_inc_value");
@@ -27,3 +65,6 @@ incButton.addEventListener("click", () => {
   currentWidth += 5; // Увеличение на 5%
   updateWidth(currentWidth);
 });
+
+
+
